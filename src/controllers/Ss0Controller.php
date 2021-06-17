8c3d6381a52188e84b8af9b1ffd3b4510ac686c4
kriss <462679766@qq.com>
2021-06-17 13:43:30 +0800

    feat: ss0

---
+++ src/controllers/Ss0Controller.php
@@ -0,0 +1,290 @@
+<?php
+
+namespace spider\controllers;
+
+use spider\service\ss0\SpiderXCX;
+use yii\console\Controller;
+use yii\console\ExitCode;
+use yii\helpers\ArrayHelper;
+
+class Ss0Controller extends Controller
+{
+    /**
+     * @var SpiderXCX
+     */
+    private $spider;
+
+    public function init()
+    {
+        $this->spider = new SpiderXCX();
+    }
+
+    // 测试
+    public function actionIndex()
+    {
+        $data = $this->spider->taskList();
+        dd($data);
+    }
+
+    // 签到
+    // php yii ss0/sign
+    public function actionSign()
+    {
+        $is = $this->checkAuth();
+        if (!$is) {
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+
+        $data = $this->spider->taskList();
+        $index = date('w');
+        $index = $index == 0 ? 6 : $index - 1;
+        if ($data['sign'][$index]['status'] == 2) {
+            $this->writeLn('已签到');
+            return ExitCode::OK;
+        }
+        $data = $this->spider->sign();
+        $this->writeLn('签到：' . $data['msg']);
+        return ExitCode::OK;
+    }
+
+    // 任务
+    // php yii ss0/task
+    public function actionTask()
+    {
+        $is = $this->checkAuth();
+        if (!$is) {
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+
+        $max = 30;
+        $count = 0;
+        $errorSolved = false;
+        while (true) {
+            if (++$count > $max) {
+                $this->writeLn('超次数结束: ' . $count);
+                break;
+            }
+
+            $data = $this->spider->taskList();
+            $tasks = $data['task'];
+            $tasksCount = count($tasks);
+            $this->writeLn("第{$count}次获取任务列表，数量：" . $tasksCount);
+
+            if ($tasksCount !== 7) {
+                if ($errorSolved === false) {
+                    $this->pickGift(3);
+                    continue;
+                }
+                $this->writeLn('任务数不正常');
+                break;
+            }
+
+            ArrayHelper::multisort($tasks, [
+                'status', // 优先获取可以领取奖励的
+                'type' // 主要为了优先处理点赞，因为点赞包括浏览
+            ], [SORT_ASC, SORT_DESC]);
+            $canPickGiftTaskId = 0;
+            $firstNotOverTask = null;
+            foreach ($tasks as $item) {
+                if ($item['status'] === 0) {
+                    // 无效
+                    continue;
+                }
+                if ($item['status'] === 1) {
+                    // 可领取奖励
+                    $canPickGiftTaskId = $item['id'];
+                    break;
+                }
+                if ($firstNotOverTask) {
+                    continue;
+                }
+                if ($item['status'] === 2) {
+                    // 已完成
+                    continue;
+                }
+                $firstNotOverTask = $item;
+            }
+            if ($canPickGiftTaskId) {
+                $this->pickGift($canPickGiftTaskId);
+                continue;
+            }
+            if ($firstNotOverTask) {
+                $this->doTaskItem($firstNotOverTask);
+                continue;
+            }
+            $this->writeLn('所有已完成');
+            break;
+        }
+
+        return ExitCode::OK;
+    }
+
+    private function checkAuth(): bool
+    {
+        $data = $this->spider->userRole();
+        if ($data['status'] == 200) {
+            $this->writeLn("角色：{$data['data']['role_name']}({$data['data']['server_name']})");
+            return true;
+        }
+        $this->writeLn($data['msg']);
+        return false;
+    }
+
+    private function doTaskItem($item)
+    {
+        $this->writeLn("开始任务：{$item['name']}:{$item['desc']}");
+        $hasCount = $item['count'];
+        switch ($item['type']) {
+            case 7:
+                // 评论阵容
+                $this->commentBattle($hasCount);
+                break;
+            case 6:
+                // 发布阵容
+                $this->submitBattle($hasCount);
+                break;
+            case 5:
+                // 分享
+                $this->share($hasCount);
+                break;
+            case 2:
+                // 点赞攻略
+                $this->viewAndPrizeArticle($hasCount, $hasCount);
+                break;
+            case 1:
+                // 浏览攻略
+                $this->viewAndPrizeArticle($hasCount, 0);
+                break;
+            default:
+                $this->writeLn('忽略');
+        }
+    }
+
+    private function pickGift(int $taskId)
+    {
+        $data = $this->spider->pickGift($taskId);
+        $this->writeLn("领取奖励[{$taskId}]：{$data['msg']}");
+    }
+
+    private $_battleSubmitData = [];
+    private $_battleSubmitDataIndex = 0;
+
+    private function submitBattle($hasCount)
+    {
+        $max = 5;
+        if ($hasCount >= $max) {
+            return;
+        }
+        if (!$this->_battleSubmitData) {
+            $playList = $this->spider->battlePlayList();
+            $heroList = $this->spider->battleHeroList();
+            $generalList = array_values($heroList['general']);
+            $strategistList = array_values($heroList['strategist']);
+            for ($i = 0; $i < $max - $hasCount; $i++) {
+                $this->_battleSubmitData[] = [
+                    $playList[array_rand($playList)]['id'],
+                    $generalList[array_rand($generalList)]['id'],
+                    [
+                        $strategistList[array_rand($strategistList)]['id'],
+                    ],
+                ];
+            }
+        }
+
+        $title = $this->randomContent();
+        $battleSubmitData = $this->_battleSubmitData[$this->_battleSubmitDataIndex];
+        $this->_battleSubmitDataIndex++;
+        $data = $this->spider->battleSubmit($title, $title, ...$battleSubmitData);
+        $this->writeLn("第{$hasCount}次：{$data['msg']}");
+
+        $this->submitBattle($hasCount);
+    }
+
+    private $_battleList = [];
+    private $_commentIndex = 0;
+
+    private function commentBattle($hasCount)
+    {
+        $max = 20;
+        if ($hasCount >= $max) {
+            return;
+        }
+
+        if (!$this->_battleList) {
+            $this->_battleList = $this->spider->battleList(1, $max);
+        }
+
+        $battleId = $this->_battleList[$this->_commentIndex]['id'];
+        $this->_commentIndex++;
+        $data = $this->spider->battleComment($battleId, $this->randomContent());
+        $this->writeLn("第{$hasCount}次：{$data['msg']}");
+
+        $this->commentBattle($hasCount + 1);
+    }
+
+    private $_articleList = [];
+    private $_articleIndex = 0;
+
+    private function viewAndPrizeArticle($viewHasCount, $prizeHasCount)
+    {
+        $viewMax = 50;
+        $prizeMax = 5;
+        $needPrize = $prizeHasCount < $prizeMax;
+        $needView = $needPrize || $viewHasCount < $viewMax;
+        if (!$needView && !$needPrize) {
+            return;
+        }
+
+        if (!$this->_articleList) {
+            $this->_articleList = $this->spider->articleList(1, max($viewMax, $prizeMax));
+        }
+
+        $articleId = $this->_articleList[$this->_articleIndex]['id'];
+        $this->_articleIndex++;
+        $data = $this->spider->articleDetail($articleId);
+        $this->writeLn("第{$viewHasCount}次：{$data['msg']}");
+        if ($needPrize) {
+            $data = $this->spider->articlePrize($articleId);
+            $this->writeLn("第{$prizeHasCount}次：{$data['msg']}");
+            $data = $this->spider->articlePrizeCancel($articleId);
+            $this->writeLn("第{$prizeHasCount}次：{$data['msg']}");
+            $prizeHasCount++;
+        }
+
+        $this->viewAndPrizeArticle($viewHasCount + 1, $prizeHasCount);
+    }
+
+    private function share($hasCount)
+    {
+        $max = 20;
+        if ($hasCount >= $max) {
+            return;
+        }
+
+        $data = $this->spider->share();
+        $this->writeLn("第{$hasCount}次：{$data['msg']}");
+
+        $this->share($hasCount + 1);
+    }
+
+    private function randomContent(): string
+    {
+        $arr = [
+            '66666',
+            '666666',
+            '6666666',
+            '66666666',
+            '666666666',
+            '88888',
+            '888888',
+            '8888888',
+            '88888888',
+        ];
+        return $arr[array_rand($arr)];
+    }
+
+    private function writeLn($msg)
+    {
+        $this->stdout($msg . "\n");
+    }
+}
---
+++ src/service/ss0/SpiderXCX.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace spider\service\ss0;
+
+use GuzzleHttp\Client;
+use yii\base\Exception;
+use yii\helpers\Json;
+
+class SpiderXCX
+{
+    private $client;
+    private $authorization;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => 'https://wx-api.youzu.com/game/374/',
+            'timeout' => 5,
+        ]);
+        $this->authorization = get_env('SS0_AUTHORIZATION');
+        if (!$this->authorization) {
+            throw new Exception('未知的 SS0_AUTHORIZATION');
+        }
+    }
+
+    public function userRole()
+    {
+        return $this->api('user/get-defaultRole', [], [
+            'resolveData' => false,
+        ]);
+    }
+
+    public function taskList()
+    {
+        return $this->api('welfare/task-list');
+    }
+
+    public function sign()
+    {
+        return $this->api('welfare/sign-gift', [], [
+            'resolveData' => false,
+        ]);
+    }
+
+    public function pickGift(int $taskId)
+    {
+        return $this->api('welfare/task-gift', [
+            'task_id' => $taskId,
+        ], [
+            'resolveData' => false,
+        ]);
+    }
+
+    public function battlePlayList()
+    {
+        return $this->api('battle/play', [
+            'num' => 8,
+        ]);
+    }
+
+    public function battleHeroList()
+    {
+        return $this->api('battle/hero-list', [
+            'hero_type' => 0,
+            'profess_type' => 0,
+        ]);
+    }
+
+    public function battleSubmit(string $title, string $content, int $playId, int $generalId, array $strategistList)
+    {
+        $detail = [
+            ['location' => 0, 'hero_id' => $generalId],
+        ];
+        for ($i = 1; $i < 17; $i++) {
+            $detail[] = ['location' => $i, 'hero_id' => $strategistList[$i - 1] ?? 0];
+        }
+        return $this->api('battle/submit', [
+            'num' => 8,
+            'title' => $title,
+            'reason' => $content,
+            'play_ids' => $playId,
+            'details' => json_encode($detail),
+        ], [
+            'resolveData' => false,
+        ]);
+    }
+
+    public function battleList(int $page = 1, int $pageSize = 5)
+    {
+        $data = $this->api('battle/battle-user-list', [
+            'play_id' => 0,
+            'page' => $page,
+            'page_size' => $pageSize,
+        ]);
+        return $data['list'];
+    }
+
+    public function battleComment(int $battleId, string $content)
+    {
+        return $this->api('battle/battle-user-comment', [
+            'battle_id' => $battleId,
+            'content' => $content,
+        ], [
+            'resolveData' => false,
+        ]);
+    }
+
+    public function articleList(int $page = 1, int $pageSize = 5)
+    {
+        $data = $this->api('article/listAndCount', [
+            'site_id' => 304,
+            'cid' => 'sdk_advance_strategy',
+            'page' => $page,
+            'limit' => $pageSize,
+            'lang_id' => 3,
+        ]);
+        return $data['list'];
+    }
+
+    public function articleDetail(int $articleId)
+    {
+        return $this->api('article/info', [
+            'site_id' => 304,
+            'id' => $articleId,
+        ]);
+    }
+
+    public function articlePrize(int $articleId)
+    {
+        return $this->api('like/article-do', [
+            'id' => $articleId,
+            'cid' => 'guide',
+        ], [
+            'resolveData' => false,
+        ]);
+    }
+
+    public function articlePrizeCancel(int $articleId)
+    {
+        return $this->api('like/article-cancel', [
+            'id' => $articleId,
+            'cid' => 'guide',
+        ], [
+            'resolveData' => false,
+        ]);
+    }
+
+    public function share()
+    {
+        return $this->api('share/share', [], [
+            'resolveData' => false,
+        ]);
+    }
+
+    private function api(string $uri, array $params = [], array $config = [])
+    {
+        $config = array_merge([
+            'resolveData' => true,
+        ], $config);
+
+        $response = $this->client->request('GET', $uri, [
+            'query' => $params,
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->authorization,
+                'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
+            ]
+        ]);
+        $data = Json::decode($response->getBody()->getContents());
+        if ($config['resolveData']) {
+            if ($data['status'] !== 200) {
+                throw new Exception($data['msg']);
+            }
+            return $data['data'];
+        }
+        return $data;
+    }
+
+
+}
